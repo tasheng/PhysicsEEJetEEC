214366c43b9b119a8d2bc47e4694a1b15acbf5c8
FHead <chen.yi.first@gmail.com>
2023-05-03 04:10:39 +0200

    more code

--- Exploration/20230502_InitialExploration/FirstExploration.cpp
+++ Exploration/20230502_InitialExploration/FirstExploration.cpp
@@ -7,13 +7,18 @@ using namespace std;
 #include "ProgressBar.h"
 #include "CommandLine.h"
 #include "Messenger.h"
+#include "JetCorrector.h"
 
 int main(int argc, char *argv[])
 {
    CommandLine CL(argc, argv);
 
-   string InputFileName = CL.Get("Input");
-   string JetTreeName   = CL.Get("Jet", "akR4ESchemeJetTree");
+   string InputFileName    = CL.Get("Input");
+   string JetTreeName      = CL.Get("Jet", "akR4ESchemeJetTree");
+   double JetR             = CL.GetDouble("JetR", 0.4);
+   vector<string> JECFiles = CL.GetStringVector("JEC");
+
+   JetCorrector JEC(JECFiles);
 
    TFile File(InputFileName.c_str());
 
@@ -35,6 +40,30 @@ int main(int argc, char *argv[])
 
       if(MParticle.PassBaselineCut() == false)
          continue;
+
+      for(int iJ = 0; iJ < MJet.nref; iJ++)
+      {
+         JEC.SetJetP(MJet.Jet[iJ].GetP());
+         JEC.SetJetE(MJet.Jet[iJ][0]);
+         JEC.SetJetTheta(MJet.Jet[iJ].GetTheta());
+         JEC.SetJetPhi(MJet.Jet[iJ].GetPhi());
+         double Correction = JEC.GetCorrection();
+         MJet.Jet[iJ] = MJet.Jet[iJ] * Correction;
+      }
+
+      for(int iJ = 0; iJ < MJet.nref; iJ++)
+      {
+         // too close to beam pipe
+         if(MJet.Jet[iJ].GetTheta() < 0.2 * M_PI || MJet.Jet[iJ].GetTheta() > 0.8 * M_PI)
+            continue;
+
+         // now we gather the particles around the jets
+         for(int iP = 0; iP < MParticle.nParticle; iP++)
+         {
+            if(GetAngle(MJet.Jet[iJ], MParticle.P[iP]) > JetR)
+               continue;
+         }
+      }
    }
    Bar.Update(EntryCount);
    Bar.Print();
