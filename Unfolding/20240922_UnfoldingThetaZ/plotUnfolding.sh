4dae7895af865c88494eafc890a3743ea5a01209
Hannah Bossi <hannah.bossi@cern.ch>
2024-09-23 06:42:55 -0400

    Initial commit of the unfolding code.

---
+++ Unfolding/20240922_UnfoldingThetaZ/plotUnfolding.sh
@@ -0,0 +1,6 @@
+./ExecuteUnfoldingStab --Input unfoldingE2C_DataUnfolding_DoubleLogBinning_09232024.root \
+        --Output StabilityCheckDataUnfolding \
+        --Label "Iteration 1","Iteration 2","Iteration 3","Iteration 4","Archived MC"\
+        --Prefix "092324" \
+        --DoRatio true \
+        --DoWeight false
